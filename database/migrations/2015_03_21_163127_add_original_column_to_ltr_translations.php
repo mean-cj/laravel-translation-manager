4c80a95603559e62a89f2eb07c577d11c2f77d21
mean-cj <register@ireallyhost.com>
2016-01-21 02:27:58 +0700

    Update 2015_03_21_163127_add_original_column_to_ltr_translations.php

--- database/migrations/2015_03_21_163127_add_original_column_to_ltr_translations.php
+++ database/migrations/2015_03_21_163127_add_original_column_to_ltr_translations.php
@@ -14,7 +14,7 @@ class AddOriginalColumnToLtrTranslations extends Migration
     public
     function up()
     {
-        Schema::table('ltm_translations', function (Blueprint $table)
+        Schema::table('ltm_translations', function ( $table)
         {
             $table->text('saved_value')->nullable();
         });
@@ -28,7 +28,7 @@ class AddOriginalColumnToLtrTranslations extends Migration
     public
     function down()
     {
-        Schema::table('ltm_translations', function (Blueprint $table)
+        Schema::table('ltm_translations', function ( $table)
         {
             $table->dropColumn('saved_value');
         });
